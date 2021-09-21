35c8116b640a6a0a5ba223813858726e5d32bd8b
masahirot <13981959+pentam19@users.noreply.github.com>
2021-09-21 22:55:07 +0900

    react-redux-restapi-sample

---
+++ api_insta/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'user'
+
+router = DefaultRouter()
+
+# ModelViewSetを継承しているView
+router.register('profile', views.ProfileViewSet)
+router.register('post', views.PostViewSet)
+router.register('comment', views.CommentViewSet)
+
+# Genericsの汎用View
+urlpatterns = [
+    path('register/', views.CreateUserView.as_view(), name='register'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+    path('', include(router.urls))
+]
